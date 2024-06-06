4eb1a9cb4cbab142953afc38ddd1ab3231074fc9
haseeb hummo <haseebpersonalwork@gmail.com>
2024-06-06 19:58:37 +0530

    feat: add random number broadcasting to WebSocket client.

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const PORT = 8080;
 const http = require('http');
+const WebSocketServer = require('websocket').server;
+
+const PORT = 8080;
 
 const server = http.createServer((req, res) => {
     res.write('haseeb bhiya web sockets sikh rahe hain...');
     res.end();
-})
+});
 
 server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
 
-// const http = require('http');
+const websocket = new WebSocketServer({
+    httpServer: server
+});
 
-// const app = require('express')();
+let clientConnection = null;
 
-// app.get('/', (req, res) => {
-//     res.send('haseeb bhiya web socket sikh rahe hain...')
-// })
+websocket.on('request', (req) => {
+    clientConnection = req.accept(null, req.origin);
 
-// app.listen(PORT, () => {
-//     console.log(`server is listning on port ${PORT}`);
-// })
+    clientConnection.on('open', () => {
+        console.log('Connection open');
+    });
+
+    clientConnection.on('close', () => {
+        console.log('Connection closed');
+        clientConnection = null; // Clear the connection when closed
+    });
+
+    clientConnection.on('message', (message) => {
+        console.log(`We received a message from client: ${message.utf8Data}`);
+    });
+
+    sendEverySeconds();
+});
+
+
+
+const sendEverySeconds = () => {
+    clientConnection.send(`${Math.floor(Math.random() * 100 + 1)}`);
+
+    setTimeout( sendEverySeconds() , 1000);
+}
