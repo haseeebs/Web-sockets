22f2967da5dffded40de4bb1365aab5d028ced28
haseeb hummo <haseebpersonalwork@gmail.com>
2024-06-10 04:45:50 +0530

    refactor: migrate code to TypeScript and Implement user management with room-based messaging functionality.

---
+++ Server/src/index.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import { WebSocketServer } from "ws";
+import path from "path";
+
+const app = express();
+app.use(express.static(path.join(__dirname, "../../Client")));
+
+const server = app.listen(8080, () => {
+  console.log("Express server is running on port 8080...");
+});
+
+const wss = new WebSocketServer({ server });
+
+const users: {
+  [key: string]: { roomId: string; ws: any };
+} = {};
+
+let count = 0;
+
+wss.on("connection", (ws, req) => {
+  const wsId = count++;
+  
+  ws.on("message", (message: string) => {
+    const data = JSON.parse(message.toString());
+
+    if (data.type === "join") {
+      users[wsId] = {
+        roomId: data.payload.roomId,
+        ws,
+      };
+    }
+
+    if (data.type === "message") {
+      const roomId = users[wsId].roomId;
+      const message = data.payload.message;
+
+      Object.keys(users).forEach(userId => {
+        if(users[userId].roomId === roomId) {
+          users[userId].ws.send(JSON.stringify({
+                type: 'message',
+                payload: { message }
+            }))
+         }
+      })
+    }
+  });
+});
